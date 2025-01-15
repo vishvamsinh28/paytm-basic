eb2b8986ebef9e23012202b5f9109d25a85737ec
Vishvamsinh Vaghela <vaghelavishvamsinh11111@gmail.com>
2025-01-15 22:45:26 +0530

    filter route added

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,13 +1,12 @@
-const express = require("express")
-const router = express.Router()
-const userRoutes = require("./user")
-const User = require("../db")
-const authenticate = require("../auth")
+const express = require("express");
+const router = express.Router();
+const userRoutes = require("./user");
+const User = require("../db");
+const authenticate = require("../auth");
 
-router.use("/user", userRoutes)
+router.use("/user", userRoutes);
 
 router.put("/user", authenticate, async (req, res) => {
-
     try {
         console.log("User ID:", req.userId);
         const updatedData = await User.updateOne({ _id: req.userId }, req.body);
@@ -15,15 +14,44 @@ router.put("/user", authenticate, async (req, res) => {
         res.json({
             msg: "Update success!",
             updatedData,
-            data: req.body
-        })
+            data: req.body,
+        });
     } catch (err) {
         res.json({
             msg: "Error while updating",
-            err
-        })
+            err,
+        });
     }
+});
 
-})
+router.get("/user/bulk", async (req, res) => {
+    const { filter } = req.query;
 
-module.exports = router
+    const users = await User.find({
+        $or: [
+            {
+                firstname: {
+                    $regex: filter,
+                },
+            },
+            {
+                lastname: {
+                    $regex: filter,
+                },
+            },
+        ],
+    });
+
+    const finalData = users.map((user) => ({
+        username: user.username,
+        firstname: user.firstname,
+        lastname: user.lastname,
+        id: user._id,
+    }));
+
+    res.json({
+        msg: finalData
+    })
+});
+
+module.exports = router;
