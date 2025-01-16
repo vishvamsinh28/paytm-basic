8be585ef1da35075c590b93eb62988cff9bfc40a
Vishvamsinh Vaghela <vaghelavishvamsinh11111@gmail.com>
2025-01-16 16:59:19 +0530

    tailwind support added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,19 @@
-export default function App(){
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Signup from "./components/Signup"
+import Signin from "./components/Signin"
+import Dashboard from "./components/Dashboard"
+import SendMoney from "./components/SendMoney"
+
+
+export default function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/send" element={<SendMoney />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
---
+++ frontend/src/components/SendMoney.jsx
@@ -0,0 +1,7 @@
+export default function SendMoney(){
+    return (
+        <div>
+            
+        </div>
+    )
+}
