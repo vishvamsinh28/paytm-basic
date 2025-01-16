de172b8482138c278517f2af506ab1665abd3d61
Vishvamsinh Vaghela <vaghelavishvamsinh11111@gmail.com>
2025-01-16 15:45:53 +0530

    used session transactions

--- backend/routes/accounts.js
+++ backend/routes/accounts.js
@@ -2,6 +2,7 @@ const express = require("express")
 const router = express.Router()
 const authenticate = require("../auth")
 const { Account, User } = require("../db")
+const mongoose = require("mongoose")
 
 router.get("/balance", authenticate, async (req, res) => {
     const account = await Account.findOne({
@@ -13,22 +14,29 @@ router.get("/balance", authenticate, async (req, res) => {
     })
 })
 
-// this is a bad solution, a good approach would be to use transaction
+// this is a good approach by using transactions to ensure that all database queries either succeed or fail together.
 router.post("/transfer", authenticate, async (req, res) => {
-    const user = await Account.findOne({userID: req.userId})
+    const session = await mongoose.startSession()
+
+    session.startTransaction()
+
+    const user = await Account.findOne({userID: req.userId}).session(session)
     const { amount, to } = req.body
-    const receiver = await User.findOne({username: to})
-    const receiverBalance = await Account.findOne({userID: receiver._id})
+    const receiver = await User.findOne({username: to}).session(session)
+    const receiverBalance = await Account.findOne({userID: receiver._id}).session(session)
 
     if(amount > user.balance || amount < 0){
+        await session.abortTransaction()
         return res.json({
             msg: "amount should be equal to or less than balance and it cannot be in negative"
         })
     }
 
-    await Account.findByIdAndUpdate(receiverBalance._id, {balance: receiverBalance.balance + amount})
-    await Account.findByIdAndUpdate(user._id, {balance:  user.balance - amount})
+    await Account.findByIdAndUpdate(receiverBalance._id, {balance: receiverBalance.balance + amount}).session(session)
+    await Account.findByIdAndUpdate(user._id, {balance:  user.balance - amount}).session(session)
 
+    await session.commitTransaction()
+    session.endSession()
     res.json({
         msg: "transfer successful"
     })
