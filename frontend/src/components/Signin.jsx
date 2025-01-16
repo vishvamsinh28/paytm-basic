68bcf8e7568e77a9ad01bd4d9f513aacfa38d847
Vishvamsinh Vaghela <vaghelavishvamsinh11111@gmail.com>
2025-01-16 17:38:39 +0530

    name and id added

--- frontend/src/components/Signin.jsx
+++ frontend/src/components/Signin.jsx
@@ -3,20 +3,24 @@ export default function Signin() {
         <div className="bg-white p-4 rounded-lg mt-16">
             <form className="flex flex-col">
                 <h1 className="font-bold text-3xl text-center">Sign In</h1>
-                <h2 className="text-gray-500 mt-2">
+                <h2 className="text-gray-500 mt-2 text-center">
                     Enter Your information to login
                 </h2>
 
-                <label className="my-4">Username</label>
+                <label className="my-4" htmlFor="un">Username</label>
                 <input
+                    name="username"
                     className="outline-0 border border-gray-300 p-2 rounded-md text-gray-500"
                     type="text"
+                    id="un"
                 ></input>
 
-                <label className="my-4">Password</label>
+                <label className="my-4" htmlFor="pass">Password</label>
                 <input
+                    name="password"
                     className="outline-0 border border-gray-300 p-2 rounded-md text-gray-500"
                     type="password"
+                    id="pass"
                 ></input>
 
                 <button onClick={"some funcion"} className="bg-black mt-6 p-2 rounded-md text-white">
--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -3,32 +3,40 @@ export default function Signup() {
         <div className="bg-white p-4 rounded-lg mt-16">
             <form className="flex flex-col">
                 <h1 className="font-bold text-3xl text-center">Sign Up</h1>
-                <h2 className="text-gray-500 mt-2">
+                <h2 className="text-gray-500 mt-2 text-center">
                     Enter Your information to create an account
                 </h2>
 
-                <label className="my-4">First Name</label>
+                <label className="my-4" htmlFor="fn">First Name</label>
                 <input
                     className="outline-0 border border-gray-300 p-2 rounded-md text-gray-500"
                     type="text"
+                    name="firstname"
+                    id="fn"
                 ></input>
 
-                <label className="my-4">Last Name</label>
+                <label className="my-4" htmlFor="ln">Last Name</label>
                 <input
                     className="outline-0 border border-gray-300 p-2 rounded-md text-gray-500"
                     type="text"
+                    name="lastname"
+                    id="ln"
                 ></input>
 
-                <label className="my-4">Username</label>
+                <label className="my-4" htmlFor="un">Username</label>
                 <input
                     className="outline-0 border border-gray-300 p-2 rounded-md text-gray-500"
                     type="text"
+                    name="username"
+                    id="un"
                 ></input>
 
-                <label className="my-4">Password</label>
+                <label className="my-4" htmlFor="pass">Password</label>
                 <input
                     className="outline-0 border border-gray-300 p-2 rounded-md text-gray-500"
                     type="password"
+                    name="password"
+                    id="pass"
                 ></input>
 
                 <button onClick={"some funcion"} className="bg-black mt-6 p-2 rounded-md text-white">
