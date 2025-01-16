bd12c7d92e867a92cae60029148f429c2028562f
Vishvamsinh Vaghela <vaghelavishvamsinh11111@gmail.com>
2025-01-16 18:47:25 +0530

    login updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Signup from "./components/Signup"
-import Signin from "./components/Signin"
+import Login from "./components/Login"
 import Dashboard from "./components/Dashboard"
 import SendMoney from "./components/SendMoney"
 
@@ -10,7 +10,7 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/signup" element={<Signup />} />
-        <Route path="/signin" element={<Signin />} />
+        <Route path="/Login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/send" element={<SendMoney />} />
       </Routes>
---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,70 @@
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
+
+export default function Login() {
+    const navigate = useNavigate();
+    const [data, setData] = useState({
+        username: "",
+        password: "",
+    });
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const apiRoute = "http://localhost:3000/api/v1/user/login";
+
+        const response = await axios.post(apiRoute, data);
+
+        if (response.data.proceed) {
+            setData({
+                username: "",
+                password: "",
+            });
+            navigate("/dashboard");
+        }
+    };
+
+    return (
+        <div className="bg-white p-4 rounded-lg mt-16">
+            <form className="flex flex-col" onSubmit={handleSubmit}>
+                <h1 className="font-bold text-3xl text-center">Sign In</h1>
+                <h2 className="text-gray-500 mt-2 text-center">
+                    Enter Your information to login
+                </h2>
+
+                <label className="my-4" htmlFor="un">Username</label>
+                <input
+                    onChange={(e) => setData({ ...data, username: e.target.value })}
+                    name="username"
+                    className="outline-0 border border-gray-300 p-2 rounded-md text-gray-500"
+                    type="text"
+                    id="un"
+                    value={data.username}
+                />
+
+                <label className="my-4" htmlFor="pass">Password</label>
+                <input
+                    onChange={(e) => setData({ ...data, password: e.target.value })}
+                    name="password"
+                    className="outline-0 border border-gray-300 p-2 rounded-md text-gray-500"
+                    type="password"
+                    id="pass"
+                    value={data.password}
+                />
+
+                <button onClick={handleSubmit} className="bg-black mt-6 p-2 rounded-md text-white">
+                    Login
+                </button>
+
+
+                <div className="flex m-4">
+                    <h3 className="text-center mx-2">Don't have an account?</h3>
+                    <a onClick={() => navigate("/signup")} className="underline">
+                        Sign Up
+                    </a>
+                </div>
+            </form>
+        </div>
+    );
+}
